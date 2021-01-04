555c6c6ed90be4c6f2612d784fb85841afc90517
ParkWonBin <57087624+ParkWonBin@users.noreply.github.com>
2021-01-04 22:54:24 +0900

    C# step4

--- C#/Beakjoon_steps/Program.cs
+++ C#/Beakjoon_steps/Program.cs
@@ -10,8 +10,10 @@ namespace Beakjoon_steps
             RunStep02();
             RunStep03();
             */
-            Step03.P05();
+            Step04.P03();
         }
+        
+        // 파일 입출력 
         static void RunStep01() {
             Step01.P01();
             Step01.P02();
@@ -19,7 +21,8 @@ namespace Beakjoon_steps
             Step01.P04();
             Step01.P05(); // ' ' 구분 숫자 2개 입력
         }
-
+        
+        // if 문
         static void RunStep02() {
             // ' ' 구분 숫자 2개 입력
             Step02.P01();
@@ -29,13 +32,21 @@ namespace Beakjoon_steps
             Console.WriteLine("공백 구분 숫자 2개 입력 바람");
             Step02.P05();
         }
-
+        
+        // for 문
         static void RunStep03() {
             Step03.P01();
             Step03.P02();
             Step03.P03();
             Step03.P04();
             Step03.P05();
+        }
+        
+        // while 문
+        static void Runstep04() {
+            Step04.P01(); // 0 0 나오면 종료
+            Step04.P02(); // EDF 나오면 종료
+            Step04.P03(); // EDF 나오면 종료
             
         }
     }
---
+++ C#/Beakjoon_steps/Step04.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Beakjoon_steps
+{
+    public class Step04
+    {
+        public static void P01() {
+            while (true) { // 0 0 입력 받으면 종료
+                string[] AB = Console.ReadLine().Split(" ");
+                int A = int.Parse(AB[0]);
+                int B = int.Parse(AB[1]);
+                if (A == 0 && B == 0) break;
+                Console.WriteLine(A + B);
+            }
+        }
+
+        public static void P02() {
+            while (true) { // EOF 나오면 종료
+                string n = Console.ReadLine();
+                if (n == null) break;
+                string[] m = n.Split();
+                int a = int.Parse(m[0]);
+                int b = int.Parse(m[1]);
+                int c = a + b;
+                Console.WriteLine(c);
+            }
+        }
+
+        public static void P03() {
+            int n = int.Parse(Console.ReadLine()!);
+            int count = 1;
+            int tmp1 = n % 10 + (n - n % 10) / 10;
+            int tmp = tmp1 % 10 + 10*(n % 10);
+            while (tmp != n) {
+                tmp = 10*(tmp % 10) + (tmp % 10 + (tmp - tmp % 10) / 10)%10;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
